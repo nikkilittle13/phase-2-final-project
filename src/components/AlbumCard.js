dd52a570752ec280cabf29346eee474eb9357770
Nikki Little <nikkilittle13@gmail.com>
2024-04-02 13:32:39 -0400

    vote button functionality working as expected

--- src/components/AlbumCard.js
+++ src/components/AlbumCard.js
@@ -1,28 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect} from "react";
 
-function AlbumCard({ albums, setAlbums }) {
-  
+function AlbumCard({ handleClick, albums }) {
 
-useEffect(() => {
-  fetch("http://localhost:3000/albums")
-  .then(res => res.json())
-  .then(data => setAlbums(data))
-}, [])
 
-return (
-  <div>
-    {albums.map(album => {
-      return (
-        <div key={album.id}>
-          <h1>{album.title}</h1>
-          <h4>Artist: {album.artist}</h4>
-          <h3>Votes: {album.votes}</h3>
-          <button>Vote Now</button>
-        </div>
-      )
-    })}
-  </div>
-)
+  return (
+    <div>
+      {albums.map((album, index) => {
+        return (
+          <div key={album.id}>
+            <h1>{album.title}</h1>
+            <h4>Artist: {album.artist}</h4>
+            <h3>Votes: {album.votes}</h3>
+            <button onClick={() => handleClick(index)}>Vote Now</button>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default AlbumCard;
--- src/pages/Albums.js
+++ src/pages/Albums.js
@@ -1,9 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "../components/NavBar";
 import AlbumCard from "../components/AlbumCard";
 
 function Albums() {
   const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/albums")
+      .then((res) => res.json())
+      .then((data) => setAlbums(data))
+      .catch((error) => console.error("Error fetching albums:", error));
+  }, []);
+
+  function handleClick(index) {
+    const album = albums[index];
+  
+    fetch(`http://localhost:3000/albums/${album.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        votes: album.votes + 1,
+      }),
+    })
+      .then((res) => res.json())
+      .then((updatedAlbum) => {
+        // Update the albums state with the updated album data
+        setAlbums((prevAlbums) => {
+          const updatedAlbums = [...prevAlbums];
+          updatedAlbums[index] = updatedAlbum;
+          return updatedAlbums;
+        });
+      })
+      .catch((error) => console.error("Error updating album:", error));
+  }
+  
 
   return (
     <div>
@@ -11,7 +43,7 @@ function Albums() {
         <NavBar />
       </header>
       <h1>Albums</h1>
-      <AlbumCard albums={albums} setAlbums={setAlbums} />
+      <AlbumCard albums={albums} handleClick={handleClick} />
     </div>
   );
 }
